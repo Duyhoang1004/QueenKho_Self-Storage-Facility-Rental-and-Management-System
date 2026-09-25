c69d0e41fcb72369fc381bb61307843db62f2552
Kayden <thanhsonnguyen1235@gmail.com>
2026-09-25 08:44:26 +0700

    chỉnh lỗi

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,39 +3,42 @@ import CustomerLayout from './layouts/CustomerLayout';
 import HomePage from './pages/home/HomePage';
 import LoginPage from './pages/login/LoginPage';
 import RegisterPage from './pages/register/RegisterPage';
+
+// Import của bạn frontend_UC12_UC13
 import MyReservationsPage from './pages/my_reservation/MyReservationsPage';
 import PendingReservationsPage from './pages/my_reservation/PendingReservationsPage';
+
+// Import của bạn nhánh main
+import CreateReservationPage from './pages/reservation/CreateReservationPage';
+import PaymentQRPage from './pages/reservation/PaymentQRPage';
+import ReservationConfirmationPage from './pages/reservation/ReservationConfirmationPage';
 
 export default function App() {
   return (
     <Routes>
       <Route path="/login" element={<LoginPage />} />
       <Route path="/register" element={<RegisterPage />} />
+      
       {/* ========== Customer Portal ========== */}
       <Route element={<CustomerLayout />}>
         <Route path="/" element={<HomePage />} />
+        
+        {/* Route của nhánh frontend_UC12_UC13 */}
         <Route path="/kho-cua-toi" element={<MyReservationsPage />} />
-        {/* 
-          Các thành viên thêm route module của mình ở đây:
-          
-          <Route path="/tim-va-dat-kho" element={<SearchPage />} />
-          <Route path="/kho-cua-toi" element={<MyStoragePage />} />
-          <Route path="/thanh-toan" element={<PaymentPage />} />
-          <Route path="/ho-tro" element={<SupportPage />} />
-        */}
+        
+        {/* Route của nhánh main */}
+        <Route path="/booking" element={<CreateReservationPage />} />
+        <Route path="/tim-va-dat-kho" element={<CreateReservationPage />} />
+        <Route path="/booking/payment" element={<PaymentQRPage />} />
+        <Route path="/booking/confirmation" element={<ReservationConfirmationPage />} />
+
       </Route>
+
       {/* ========== UC-13: FM Pending Reservations (tạm standalone, chưa có ManagerLayout) ========== */}
       <Route path="/fm/pending-reservations" element={<PendingReservationsPage />} />
+
       {/* 
         ========== Các layout khác sẽ thêm sau ==========
-        
-        <Route element={<StaffLayout />}>
-          ...
-        </Route>
-
-        <Route element={<ManagerLayout />}>
-          ...
-        </Route>
       */}
     </Routes>
   );
